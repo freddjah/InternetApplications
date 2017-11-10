c9732e71c67b02b561083e6b7dad4f2dbffb0bd2
Fredrik Eklööf <fredrik.ekloof@gmail.com>
2017-11-10 19:30:57 +0100

    initial commit

---
+++ Seminar2/header.php
@@ -0,0 +1,20 @@
+<section class="header-modal">
+    <div class="user-actions">
+        <ul>
+            <li><a href="login.php">Log in</a></li>
+            <li><a href="signup.php">Sign up</a></li>
+        </ul>
+    </div>
+    <h1>
+        Recipe.com
+    </h1>
+</section>
+
+<nav class="menu">
+    <ul>
+        <li id="active"><a href="index.html">Home</a></li>
+        <li><a href="calendar.html">Recipe Calendar</a></li>
+        <li><a href="meatballs-recipe.html">Meatballs Recipe</a></li>
+        <li><a href="pancakes-recipe.html">Pancakes Recipe</a></li>
+    </ul>
+</nav>
---
+++ Seminar2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Recipes.com</title>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+        <link rel="stylesheet" href="styles/reset.css" type="text/css">
+        <link rel="stylesheet" href="styles/base.css" type="text/css">
+        <link rel="stylesheet" href="styles/index.css" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php include_once 'header.php' ?>
+        <section class="content-wrapper">
+            Hej hej
+        </section>
+        <section class="login-form">
+            <form action="" method="post">
+                <input type="text" name="uid" placeholder="Username">
+                <input type="password" name="pwd" placeholder="Password">
+                <button class="button" type="submit" name="submit">Log in</button>
+            </form>
+            <a href="signup.php">Sign up</a>
+        </section>
+    </body>
+</html>
